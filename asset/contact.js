a312a9d5f83e8d1e10961626a389149ffefeccd9
jaemin96 <lemon__96@naver.com>
2023-02-15 01:14:35 +0900

    Fix: Update asset - contact, skill
    Github issue #3 #4

--- asset/contact.js
+++ asset/contact.js
@@ -1,8 +1,8 @@
 export const Contact = {
-  email: '',
-  phoneNumber: '',
+  email: 'lemon__96@naver.com',
+  phoneNumber: '010-4731-9889',
   twitter: '',
-  github: '',
-  notion: '',
+  github: 'https://github.com/jaemin96',
+  notion: 'https://jaemdev.notion.site/5740d18ee8f342aa935360302d8db91b',
   instagram: '',
 };
--- asset/skill.js
+++ asset/skill.js
@@ -1,8 +1,6 @@
 export const Skills = {
-    client = [
-        {title: "", thumbnail: "", description: "", capacity: 0}
-    ],
-    server = [],
-    infra = [],
-    tools =[],
-}
+  client: [{ title: '', thumbnail: '', description: '', capacity: 0 }],
+  server: [],
+  infra: [],
+  tools: [],
+};

092f514a3f28e04da18435f97cf2d9bdf17070da
jaemin96 <lemon__96@naver.com>
2023-02-17 00:47:25 +0900

    Chore: Update asset - contact social list

--- asset/contact.js
+++ asset/contact.js
@@ -1,8 +1,9 @@
 export const Contact = {
   email: 'lemon__96@naver.com',
   phoneNumber: '010-4731-9889',
-  twitter: '',
-  github: 'https://github.com/jaemin96',
-  notion: 'https://jaemdev.notion.site/5740d18ee8f342aa935360302d8db91b',
-  instagram: '',
+
+  social: [
+    { key: 'github', value: 'github', url: 'https://github.com/jaemin96' },
+    { key: 'notion', value: 'notion', url: 'https://jaemdev.notion.site/5740d18ee8f342aa935360302d8db91b' },
+  ],
 };

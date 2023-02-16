30beefb26fdc15c72dc30caf38714cfa8f49fd5f
jaemin96 <lemon__96@naver.com>
2023-02-17 00:46:37 +0900

    Feat: Add util - link

---
+++ utils/alink.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export const Alink = (props) => {
+  const { url, children } = props;
+
+  return (
+    <Link href={`${url}`} target="_blank">
+      {...children}
+    </Link>
+  );
+};
---
+++ utils/index.js
@@ -0,0 +1,3 @@
+import { Alink } from './alink';
+
+export { Alink };

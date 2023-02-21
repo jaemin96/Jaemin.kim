9154d75360733c90a392d533470af8ada2ebaa4e
jaemin96 <lemon__96@naver.com>
2023-02-21 23:56:41 +0900

    Fix: Update contacts layer
    Github issue #3 #4

--- components/header/contacts.jsx
+++ components/header/contacts.jsx
@@ -4,30 +4,34 @@ import { Alink } from '../../utils';
 export const Contacts = (props) => {
   console.log({ Contact });
   return (
-    <ul className="contacts">
-      <li className="contacts_phone">
-        <img src="/assets/image/Tel.svg" alt="phone" />
-        <div>⎟</div>
-        <span>{Contact?.phoneNumber}</span>
-      </li>
-      <li className="contacts_email">
-        <img src="/assets/image/Email.svg" alt="email" />
-        <div>⎟</div>
-        <span>{Contact?.email}</span>
-      </li>
-      <li className="contacts_social">
-        <ul>
-          {Contact?.social?.map((element) => {
-            return (
-              <li key={element?.key}>
-                <Alink url={element?.url}>
-                  <img src={element?.src} alt={element?.value} />
-                </Alink>
-              </li>
-            );
-          })}
-        </ul>
-      </li>
-    </ul>
+    <>
+      <ul className="contacts">
+        <li className="contacts_phone">
+          <div className="before">
+            <img src="/assets/image/Tel.svg" alt="phone" />
+            <div>|</div>
+          </div>
+          <span>{Contact?.phoneNumber}</span>
+        </li>
+        <li className="contacts_email">
+          <div className="before">
+            <img src="/assets/image/Email.svg" alt="email" />
+            <div>|</div>
+          </div>
+          <span>{Contact?.email}</span>
+        </li>
+      </ul>
+      <ul className="contacts_social">
+        {Contact?.social?.map((element) => {
+          return (
+            <li key={element?.key}>
+              <Alink url={element?.url}>
+                <img src={element?.src} alt={element?.value} />
+              </Alink>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };

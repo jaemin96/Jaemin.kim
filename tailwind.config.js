abbe409d7427ffdaa4da6d6a1f9918459204153e
jaemin <lemon__96@naver.com>
2023-07-07 18:31:40 +0900

    feat: tailwind 설정 추가 - @media

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
     './containers/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      sm: { max: '767px' },
+      md: { min: '768px', max: '1023px' },
+      lg: { min: '1024px' },
+    },
     extend: {},
   },
   plugins: [],

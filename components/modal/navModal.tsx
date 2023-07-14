3652d17490693bdf5d78afec19ac61b2b570b630
jaemin <lemon__96@naver.com>
2023-07-14 17:09:30 +0900

    fix: navModal email & github icon 추가 및 email 기능 추가

--- components/modal/navModal.tsx
+++ components/modal/navModal.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Email2Icon, GithubIcon } from '../icon';
 import { Navigator } from '../navigator';
+import { useState, useEffect } from 'react';
 
 type NavModalProps = {
   closeModal: () => void;
@@ -7,13 +9,33 @@ type NavModalProps = {
 
 export const NavModal = (props: NavModalProps) => {
   const { closeModal } = props;
+  const [email, setEmail] = useState<string>('');
+
+  useEffect(() => {
+    setEmail('lemon__96@naver.com');
+  }, []);
+
   return (
     <div className="modal-nav w-[100%]">
-      <div className="flex justify-between p-[1.5rem]">
+      <div className="modal-nav-header flex justify-between p-[1.5rem]">
         <h1>Modal Content</h1>
         <button onClick={closeModal}>X</button>
       </div>
-      <Navigator option="flex-col items-center" />
+      <div className="modal-nav-content">
+        <Navigator option="flex-col items-center" />
+      </div>
+      <div className="modal-nav-footer">
+        <ul className="contact-items">
+          <li>
+            <GithubIcon width={25} height={25} />
+          </li>
+          <li>
+            <a href={`mailto:${email}`}>
+              <Email2Icon width={25} height={25} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };

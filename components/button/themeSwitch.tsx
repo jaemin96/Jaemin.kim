52f5ddf380958c7abef23e9be585fd2e79f56916
jaemin <lemon__96@naver.com>
2023-11-07 12:31:30 +0900

    feat & style: theme switch button - 상태관리 로직 추가 & 스타일 적용
    Github issue #15

--- components/button/themeSwitch.tsx
+++ components/button/themeSwitch.tsx
@@ -1,31 +1,48 @@
 import Image from 'next/image';
-
-/*TODO:
- * 1. 모드 변경 이미지를 클릭했을 때 팝업의 상태를 on/off 할수있도록 []
- * 2. 모드 리스트를 클릭했을 때 현재 선택된 모드 상태관리 []
- * 3. 모드 팝업 스타일링 (테두리, 그림자, 레이아웃 전체적인 부분을 의미) []
- */
+import { useState, createContext, useContext } from 'react';
 
 export const ThemeSwitch = () => {
-  // TODO: Set - dark & light set function 추가
+  // popup state
+  const [visible, setVisible] = useState<boolean>(false);
+
+  // theme mode state
+  const [theme, setTheme] = useState<string>('light');
+  const themeContext = createContext(theme);
+  const currentTheme = useContext(themeContext);
+
+  const handleVisible = () => {
+    setVisible(!visible);
+  };
+
+  const handleThemeMode = (mode: string) => {
+    try {
+      setTheme(mode);
+    } catch {
+      console.log('error');
+    }
+    setVisible(false);
+  };
+
   return (
-    // TODO: 팝업창과 버튼을 가지고 있는 모듈 컴포넌트를 생성해야함.
     <div className="absolute top-0 right-0">
       <Image
-        className="relative mx-[1rem]"
-        src={`/images/sun-eye.png`}
-        alt="라이트 모드"
+        onClick={handleVisible}
+        className="relative mx-[1rem] cursor-pointer"
+        src={`${currentTheme === 'light' ? '/images/sun-eye.png' : '/images/sun-glasses.png'}`}
+        alt="화면모드"
         width={40}
         height={40}
         priority={true}
       />
       {/* 모드 변경 팝업창 레이아웃 */}
-      <ul className="absolute top-0 right-0 mt-[3.5rem] flex flex-col font-bold theme_switch-popup m-[1rem] border w-[10rem] z-50 bg-gray-cool-1">
+      <ul
+        className={`${
+          visible || 'hidden'
+        } absolute top-0 right-0 mt-[3.5rem] flex flex-col font-bold theme_switch-popup m-[1rem] border w-[10rem] z-50 bg-gray-cool-1 shadow-lg rounded-lg`}
+      >
         <li
-          onClick={() => {
-            console.log('라이트 모드');
-          }}
-          className="flex gap-2 justify-center align-center w-full h-[100%] px-[1rem] py-[0.7rem] "
+          onClick={() => handleThemeMode('light')}
+          className="flex gap-2 justify-center align-center w-full h-[100%] px-[1rem] py-[0.7rem] hover:bg-gray-warm-2 border-b-[0.1rem] rounded-t-lg cursor-pointer"
         >
           <Image
             className="object-contain"
@@ -39,10 +56,8 @@ export const ThemeSwitch = () => {
           <span className="leading-loose">light mode</span>
         </li>
         <li
-          onClick={() => {
-            console.log('다크 모드');
-          }}
-          className="flex gap-2 justify-center align-center w-full px-[1rem] py-[0.7rem]"
+          onClick={() => handleThemeMode('dark')}
+          className="flex gap-2 justify-center align-center w-full px-[1rem] py-[0.7rem] hover:bg-gray-warm-2 rounded-b-lg cursor-pointer"
         >
           <Image
             className="object-contain"

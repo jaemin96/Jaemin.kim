b62c7bf07289c0fecdac705a1635370632f6a147
jaemin <lemon__96@naver.com>
2023-11-10 16:55:51 +0900

    style: 반응형 스타일 수정 - 모바일 환경 레이아웃 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <Head>
         <title>Jaemin.kim</title>
       </Head>
-      <div className="grid grid-cols-4 grid-rows-1 sm:grid-cols-1 sm:grid-rows-4 h-[100vh] sm:w-full sm:mx-auto">
+      <div className="grid grid-cols-4 grid-rows-1 sm:grid-cols-1 sm:grid-rows-5 h-[100vh] sm:w-full sm:mx-auto">
         <Side />
         <Content />
       </div>

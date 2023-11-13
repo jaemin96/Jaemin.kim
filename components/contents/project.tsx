c71a9b27638ea7d86ea57cb60a5cd4da48be63d7
jaemin <lemon__96@naver.com>
2023-11-13 22:04:05 +0900

    feat: project 기본 레이아웃 추가
    Github issue #17

--- components/contents/project.tsx
+++ components/contents/project.tsx
@@ -1,11 +1,36 @@
+import { ReactNode } from 'react';
 import { ContentTitle, ContentWrapper } from './module';
+
+type MyProjectProps = {
+  thumbnail: string;
+  children: ReactNode;
+};
+
+const MyProject = ({ thumbnail, children }: MyProjectProps) => {
+  return (
+    <div className="project_container flex border-2">
+      <div className="thumbnail_wrapper w-[12rem] h-[10rem] border-2">{thumbnail}</div>
+      <div className="info_wrapper">{children}</div>
+    </div>
+  );
+};
 
 export const Project = () => {
   return (
     <div id="project" className="sm:flex sm:flex-col sm:items-center mt-[3rem] mb-[1rem]">
       <ContentTitle>Project</ContentTitle>
       <ContentWrapper>
-        <div>hello</div>
+        <MyProject thumbnail="@">
+          <div className="flex">
+            <h1 className="title">Project 1</h1>
+            <span>2022 ~ 2033</span>
+          </div>
+          <ul className="skill_wrapper flex">
+            <li>react</li>
+            <li>ts</li>
+            <li>scss</li>
+          </ul>
+        </MyProject>
       </ContentWrapper>
     </div>
   );

d0d6b6d84c238734d25cd1b5bf07ed6710793940
jaemin <lemon__96@naver.com>
2023-11-14 22:05:30 +0900

    feat: project - 프로젝트 기본 레이아웃 작업 - type & interface 설정
    Github issue #17

--- components/contents/project.tsx
+++ components/contents/project.tsx
@@ -1,16 +1,51 @@
-import { ReactNode } from 'react';
+import { Alink } from '@/utils';
 import { ContentTitle, ContentWrapper } from './module';
+
+// TODO: 사용 기술 스타일 정의, MyProject 컴포넌트 모듈 분리
+
+interface Links {
+  type: string;
+  url: string;
+}
 
 type MyProjectProps = {
   thumbnail: string;
-  children: ReactNode;
+  title: string;
+  skills: string[];
+  links: Links[];
+  comment: string;
 };
 
-const MyProject = ({ thumbnail, children }: MyProjectProps) => {
+const MyProject = ({ thumbnail, skills, title, links, comment }: MyProjectProps) => {
   return (
-    <div className="project_container flex border-2">
-      <div className="thumbnail_wrapper w-[12rem] h-[10rem] border-2">{thumbnail}</div>
-      <div className="info_wrapper">{children}</div>
+    <div className="project_container sm:w-[28rem] flex border-2 mb-[1rem] last:mb-0">
+      <div className="thumbnail_wrapper w-[12rem] h-[10rem] border-2 sm:w-[18rem]">{thumbnail}</div>
+      <div className="info_wrapper m-[0.5rem] sm:w-full">
+        <div className="flex">
+          <h1 className="title">{title}</h1>
+        </div>
+        <div className="">{comment}</div>
+        <div className="flex gap-1">
+          <span>사용 기술 :</span>
+          <ul className="skill_wrapper flex gap-1">
+            {skills?.map((skill, i) => {
+              return <li key={`${i}`}>{skill}</li>;
+            })}
+          </ul>
+        </div>
+        <div className="flex gap-1">
+          <span>관련 링크 :</span>
+          <ul className="flex gap-1">
+            {links?.map((item, i) => {
+              return (
+                <li key={i}>
+                  <Alink url={`${item?.url}`}>{item?.type}</Alink>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
@@ -20,17 +55,26 @@ export const Project = () => {
     <div id="project" className="sm:flex sm:flex-col sm:items-center mt-[3rem] mb-[1rem]">
       <ContentTitle>Project</ContentTitle>
       <ContentWrapper>
-        <MyProject thumbnail="@">
-          <div className="flex">
-            <h1 className="title">Project 1</h1>
-            <span>2022 ~ 2033</span>
-          </div>
-          <ul className="skill_wrapper flex">
-            <li>react</li>
-            <li>ts</li>
-            <li>scss</li>
-          </ul>
-        </MyProject>
+        <MyProject
+          thumbnail="@"
+          title="웹 포트폴리오 페이지"
+          comment="프로젝트 소개 1"
+          skills={['react', 'scss', 'ts']}
+          links={[
+            { type: 'Github', url: 'github.com' },
+            { type: 'Notion', url: 'notion.so' },
+          ]}
+        />
+        <MyProject
+          thumbnail="@"
+          title="커넥트월렛"
+          comment="프로젝트 소개 2"
+          skills={['react', 'scss', 'ts']}
+          links={[
+            { type: 'Github', url: 'github.com' },
+            { type: 'Notion', url: 'notion.so' },
+          ]}
+        />
       </ContentWrapper>
     </div>
   );

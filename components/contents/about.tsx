31b5c8115f91f05263dce593b3399af921462bf6
jaemin <lemon__96@naver.com>
2023-11-14 22:04:46 +0900

    style: 스타일 수정 - about & contentTitle

--- components/contents/about.tsx
+++ components/contents/about.tsx
@@ -2,7 +2,7 @@ import { ContentTitle, ContentWrapper } from './module';
 
 export const About = () => {
   return (
-    <div id="about" className="sm:flex sm:flex-col sm:items-center mt-[3rem] sm:mt-0 mb-[1rem]">
+    <div id="about" className="sm:flex sm:flex-col sm:items-center mt-[3rem] sm:mt-[2rem] mb-[1rem]">
       <ContentTitle>About Me</ContentTitle>
       <ContentWrapper>
         <div>hello</div>
--- components/contents/module/contentTitle.tsx
+++ components/contents/module/contentTitle.tsx
@@ -7,7 +7,7 @@ type ContentTitleProps = {
 export const ContentTitle = ({ children }: ContentTitleProps) => {
   return (
     <div className="title-wrapper">
-      <h1 id="content-title" className="font-bold text-xl sm:text-[1.5rem] mb-[1rem]">
+      <h1 id="content-title" className="font-bold text-xl sm:text-[1.5rem] mb-[1.5rem]">
         {children}
       </h1>
     </div>

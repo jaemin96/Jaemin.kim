80b58afdb60852ed1f134a5bc2df2c390fce317c
jaemin <lemon__96@naver.com>
2023-11-15 21:04:55 +0900

    fix: progress bar 기본 세팅 세부 스타일링은 계획 중

--- components/progress/progressBar.tsx
+++ components/progress/progressBar.tsx
@@ -4,9 +4,11 @@ type ProgressBarProps = {
 
 export const ProgressBar = ({ percentage }: ProgressBarProps) => {
   return (
-    <div className="progress-container">
-      <div className={`progress-bar `}></div>
-      <span>{percentage}%</span>
+    <div className="flex gap-1 items-center m-1">
+      <div className="progress-container bg-gray-cool-2 w-[13rem] h-[1rem] rounded-full">
+        <div className={`progress-bar h-full w-[${percentage}%] bg-black-base rounded-full`}></div>
+      </div>
+      <span className="text-gray-cool-5">{percentage}%</span>
     </div>
   );
 };

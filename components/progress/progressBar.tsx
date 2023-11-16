230325ff7e25e6bf5b73d50fb107423cd1951eef
jaemin <lemon__96@naver.com>
2023-11-16 21:14:17 +0900

    style: progress bar 스타일 수정 - 색상 & 스타일

--- components/progress/progressBar.tsx
+++ components/progress/progressBar.tsx
@@ -5,10 +5,12 @@ type ProgressBarProps = {
 export const ProgressBar = ({ percentage }: ProgressBarProps) => {
   return (
     <div className="flex gap-1 items-center m-1">
-      <div className="progress-container bg-gray-cool-2 w-[13rem] h-[1rem] rounded-full">
-        <div className={`progress-bar h-full w-[${percentage}%] bg-black-base rounded-full`}></div>
+      <div className="progress-container bg-gray-cool-2 w-[13rem] h-[0.5rem] rounded-full">
+        <div
+          className={`progress-bar h-full w-[${percentage}%] bg-gradient-to-r from-info-200 bg-info-600 rounded-full`}
+        ></div>
       </div>
-      <span className="text-gray-cool-5">{percentage}%</span>
+      <span className="text-gray-cool-5 text-[0.8rem]">{percentage}%</span>
     </div>
   );
 };

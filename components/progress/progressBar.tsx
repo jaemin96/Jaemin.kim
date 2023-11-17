afad5d117767d55b0392d111175b1860ee695819
jaemin <lemon__96@naver.com>
2023-11-17 18:40:31 +0900

    fix: progress bar 고유성 작업 - 컴포넌트가 렌더링 될때마다 고유성 부여

--- components/progress/progressBar.tsx
+++ components/progress/progressBar.tsx
@@ -1,15 +1,18 @@
+import { useEffect } from 'react';
 type ProgressBarProps = {
   percentage: number;
+  id: string;
 };
 
-export const ProgressBar = ({ percentage }: ProgressBarProps) => {
+export const ProgressBar = ({ id, percentage }: ProgressBarProps) => {
+  useEffect(() => {
+    document.getElementById(id).style.width = `${percentage}%`;
+  }, [id, percentage]);
+
   return (
     <div className="flex gap-1 items-center m-1">
       <div className="progress-container bg-gray-cool-2 w-[13rem] h-[0.5rem] rounded-full">
-        <div
-          id={`progress-bar-${percentage}`}
-          className={`progress-bar h-full w-[${percentage}%] bg-gradient-to-r from-info-200 bg-info-600 rounded-full`}
-        ></div>
+        <div id={id} className={`progress-bar h-full bg-gradient-to-r from-info-200 bg-info-600 rounded-full`}></div>
       </div>
       <span className="text-gray-cool-5 text-[0.8rem]">{percentage}%</span>
     </div>

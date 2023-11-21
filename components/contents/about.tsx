80f15c701fd1b33a3b7c3402343955add46fd39c
jaemin <lemon__96@naver.com>
2023-11-21 21:45:06 +0900

    fix: about me & work experience 스타일 수정
    Github issue #17

--- components/contents/about.tsx
+++ components/contents/about.tsx
@@ -5,7 +5,16 @@ export const About = () => {
     <div id="about" className="sm:flex sm:flex-col sm:items-center mt-[3rem] sm:mt-[2rem] mb-[1rem]">
       <ContentTitle>About Me</ContentTitle>
       <ContentWrapper>
-        <div>hello</div>
+        <div>
+          <p className="sm:text-center sm:text-[1rem] font-light leading-7">
+            <span className="font-medium">기록</span>하고 정리하는 것을 좋아하는{' '}
+            <span className="font-medium">3년차 개발자 김재민</span> 입니다. <br />
+            <span className="font-medium">React</span>를 주로 사용하여 <span className="font-medium">웹 FE개발자</span>
+            로서의 경험을 쌓고 있습니다.
+            <br />
+            새로운 기술이나 문법들을 배우고 공유할 수 있는 개발 문화를 지향합니다.
+          </p>
+        </div>
       </ContentWrapper>
     </div>
   );
--- components/contents/experience.tsx
+++ components/contents/experience.tsx
@@ -10,7 +10,7 @@ type TimelineItemProps = {
 const TimelineItem = ({ act, during, children }: TimelineItemProps) => {
   return (
     <div>
-      <div>
+      <div className="sm:flex sm:gap-2 sm:min-w-[25rem] sm:">
         <h1>{act}</h1>
         <h2>{during}</h2>
       </div>
@@ -22,15 +22,13 @@ const TimelineItem = ({ act, during, children }: TimelineItemProps) => {
 export const Experience = () => {
   return (
     <div id="experience" className="sm:flex sm:flex-col sm:items-center mt-[3rem] mb-[1rem]">
-      <ContentTitle>Experience</ContentTitle>
+      <ContentTitle>Work Experience</ContentTitle>
       <ContentWrapper>
         {/* 메인타이틀(회사명 OR 활동주제)과 리스트영역(업무내역) 작성 */}
         {/* 스타일은 좌측에 연대기가 진행중인 것처럼 표시하도록 구현 */}
         <TimelineItem act="(주)하버엑스" during="21. 06 ~ 23. 09">
           <ul>
-            <li>temp text 1</li>
-            <li>temp text 2</li>
-            <li>temp text 3</li>
+            <li>work list</li>
           </ul>
         </TimelineItem>
       </ContentWrapper>

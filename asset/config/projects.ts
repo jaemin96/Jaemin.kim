cec499534c1c8001dd04f94cf917ae3e3de3af5b
jaemin <lemon__96@naver.com>
2023-11-24 16:32:24 +0900

    feat: projects config 파일 추가

---
+++ asset/config/projects.ts
@@ -0,0 +1,29 @@
+interface Links {
+  type: string;
+  url: string;
+}
+
+interface ProjectInfo {
+  key: string;
+  type: 'FE' | 'BE' | 'App';
+  thumbnail: string;
+  title: string;
+  skills: string[];
+  links: Links[];
+  comment: string;
+}
+
+export const Projects: ProjectInfo[] = [
+  {
+    key: 'web-portfolio',
+    type: 'FE',
+    title: '웹 포트폴리오 페이지',
+    comment: '개인 웹 포트폴리오 페이지 제작',
+    skills: ['next.js', 'tailwindcss', 'ts'],
+    thumbnail: '/images/project-portfolio_thumbnail.png',
+    links: [
+      { type: 'Github', url: 'https://github.com/jaemin96/Jaemin.kim' },
+      { type: 'Notion', url: 'https://www.notion.so/4e8f3668611945ab8ab9c33a5e955475?pvs=4' },
+    ],
+  },
+];

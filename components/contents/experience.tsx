7a5a5e5f378abf84a3cb4d8712a88846e5cc5542
jaemin <lemon__96@naver.com>
2023-11-24 17:22:15 +0900

    refactor: experience TimelineItem 로직 개선

--- components/contents/experience.tsx
+++ components/contents/experience.tsx
@@ -1,17 +1,18 @@
 import { ContentTitle, ContentWrapper } from './module';
 import { ReactNode } from 'react';
+import { WorkExperiences } from '@/asset/config';
 
 type TimelineItemProps = {
-  act: string;
+  company: string;
   during: string;
   children: ReactNode;
 };
 
-const TimelineItem = ({ act, during, children }: TimelineItemProps) => {
+const TimelineItem = ({ company, during, children }: TimelineItemProps) => {
   return (
     <div>
       <div className="sm:flex sm:gap-2  sm:">
-        <h1>{act}</h1>
+        <h1>{company}</h1>
         <h2>{during}</h2>
       </div>
       <div>{children}</div>
@@ -24,13 +25,30 @@ export const Experience = () => {
     <div id="experience" className="sm:flex sm:flex-col sm:items-center mt-[3rem] mb-[1rem]">
       <ContentTitle>Work Experience</ContentTitle>
       <ContentWrapper>
-        {/* 메인타이틀(회사명 OR 활동주제)과 리스트영역(업무내역) 작성 */}
-        {/* 스타일은 좌측에 연대기가 진행중인 것처럼 표시하도록 구현 */}
-        <TimelineItem act="(주)하버엑스" during="21. 06 ~ 23. 09">
-          <ul>
-            <li>work list</li>
-          </ul>
-        </TimelineItem>
+        {WorkExperiences?.map((data) => {
+          return (
+            <TimelineItem key={data?.key} company={data?.company} during={data?.during}>
+              {data?.projects &&
+                data.projects.map((project) => {
+                  return (
+                    <div key={project?.key}>
+                      <h1>{project?.title}</h1>
+                      {project?.myAct?.map((act, i) => {
+                        return <li key={i}>{act}</li>;
+                      })}
+                    </div>
+                  );
+                })}
+              {data?.contributes && (
+                <ul>
+                  {data.contributes.map((contribute, i) => {
+                    return <li key={i}>{contribute}</li>;
+                  })}
+                </ul>
+              )}
+            </TimelineItem>
+          );
+        })}
       </ContentWrapper>
     </div>
   );

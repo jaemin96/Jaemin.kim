8fc69c60892066d31d9975a024590d24b231a4e3
jaemin <lemon__96@naver.com>
2023-12-04 11:58:36 +0900

    fix: error 수정 - use <div> in <p>

--- components/contents/about.tsx
+++ components/contents/about.tsx
@@ -18,16 +18,16 @@ export const About = () => {
       <ContentTitle>Intro</ContentTitle>
       <ContentWrapper>
         <div>
-          <p className="sm:text-[0.9rem] leading-7">
-            <div>
+          <div className="sm:text-[0.9rem] leading-7">
+            <p>
               <Highlight>기록</Highlight>하고 정리하는 것을 좋아하는
               <Highlight> 3년차 개발자 김재민</Highlight> 입니다.
-            </div>
-            <div>
+            </p>
+            <p>
               <Highlight> React</Highlight>를 주로 사용하여 <Highlight>웹 FE개발자</Highlight>
               로서의 경험을 쌓고 있습니다. 새로운 기술이나 문법들을 배우고 공유할 수 있는 개발 문화를 지향합니다.
-            </div>
-          </p>
+            </p>
+          </div>
         </div>
       </ContentWrapper>
     </div>

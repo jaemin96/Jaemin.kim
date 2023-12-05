b5b08f633cbb1178f7db484c32b275d5e0b05a6d
jaemin <lemon__96@naver.com>
2023-12-05 20:21:25 +0900

    feat: intro 텍스트 모바일 화면 줄바뀜 문제 개선 - MobileBr 컴포넌트 추가

--- components/contents/about.tsx
+++ components/contents/about.tsx
@@ -13,18 +13,29 @@ export const About = () => {
     return <span className={`font-bold ${theme === 'dark' ? 'text-custom-Emerald' : ''}`}>{children}</span>;
   };
 
+  const MobileBr = () => {
+    return (
+      <span className="hidden sm:inline">
+        <br />
+      </span>
+    );
+  };
+
   return (
     <div id="about" className="sm:flex sm:flex-col sm:items-center sm:text-center mt-[3rem] sm:mt-[2rem] mb-[1rem]">
       <ContentTitle>Intro</ContentTitle>
       <ContentWrapper>
         <div>
           <div className="sm:text-[0.9rem] leading-7">
-            <p className="whitespace-nowrap">
+            <p>
               <Highlight>기록</Highlight>하고 정리하는 것을 좋아하는
+              <MobileBr />
               <Highlight> 3년차 개발자 김재민</Highlight> 입니다.
             </p>
             <p className="whitespace-nowrap">
-              <Highlight> React</Highlight>를 주로 사용하여 <Highlight>웹 FE개발자</Highlight>
+              <Highlight> React</Highlight>를 주로 사용하여
+              <MobileBr />
+              <Highlight>웹 FE개발자</Highlight>
               로서의 경험을 쌓고 있습니다.
             </p>
           </div>

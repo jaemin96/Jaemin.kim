c93ed07758a07b53b3fb302f42c59cebc3a23fbf
jaemin <lemon__96@naver.com>
2023-12-11 16:11:56 +0900

    chore: config 정보 수정 - notion 주소 업데이트 & 텍스트 수정

--- hooks/useContacts.tsx
+++ hooks/useContacts.tsx
@@ -1,19 +1,17 @@
 import { useState } from 'react';
 
 type Contacts = {
-  tel: string;
   email: string;
   github: string;
   notion: string;
 };
 
 const useContacts = (): Contacts => {
-  const [tel] = useState('010-4731-9889');
   const [email] = useState('mailto:lemon__96@naver.com');
   const [github] = useState('https://github.com/jaemin96');
-  const [notion] = useState('https://www.notion.so/portfolio-jaemin/11bd18125d0c462db8356ee877bce503');
+  const [notion] = useState('https://receptive-option-330.notion.site/FE-2e8bee584473476696917dcb1e9198c2?pvs=4');
 
-  return { tel, email, github, notion };
+  return { email, github, notion };
 };
 
 export default useContacts;

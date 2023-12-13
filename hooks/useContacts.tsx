889b1d289bfae714d9f5a455f14183eaf01d5d55
jaemin <lemon__96@naver.com>
2023-12-13 16:34:26 +0900

    chore: update useContacts - notion URL
    Github issue #24

--- hooks/useContacts.tsx
+++ hooks/useContacts.tsx
@@ -9,7 +9,7 @@ type Contacts = {
 const useContacts = (): Contacts => {
   const [email] = useState('mailto:lemon__96@naver.com');
   const [github] = useState('https://github.com/jaemin96');
-  const [notion] = useState('https://receptive-option-330.notion.site/FE-2e8bee584473476696917dcb1e9198c2?pvs=4');
+  const [notion] = useState('https://receptive-option-330.notion.site/2e8bee584473476696917dcb1e9198c2?pvs=4');
 
   return { email, github, notion };
 };
